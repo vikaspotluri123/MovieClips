3543ba0d5b38863708ac09dac446fe289226d9d8
Vikas <vikaspotluri123.github@gmail.com>
2019-01-23 19:20:32 -0600

    code format: xo (just index.js)

--- index.js
+++ index.js
@@ -1,103 +1,85 @@
-const path = require('path'),
-    glob = require('glob'),
-    electron = require('electron'),
-    autoUpdater = require('./autoUpdater'),
+const path = require('path');
+const {BrowserWindow, app} = require('electron');
+// @const autoUpdater = require('./autoUpdater');
 
-    BrowserWindow = electron.BrowserWindow,
-    app = electron.app
-
-var debug = /--debug/.test(process.argv[2]);
+let debug = /--debug/.test(process.argv[2]);
 debug = true;
 
-if (process.mas) app.setName('Electron APIs')
+if (process.mas) {
+	app.setName('Electron APIs');
+}
 
-var mainWindow = null
+let mainWindow = null;
 
-function initialize()
-{
-    var shouldQuit = makeSingleInstance()
-    if (shouldQuit) return app.quit()
+function initialize() {
+	const shouldQuit = makeSingleInstance();
+	if (shouldQuit) {
+		return app.quit();
+	}
 
-    function createWindow()
-    {
-        var windowOptions =
-        {
-            width: 1080,
-            minWidth: 680,
-            minHeight:680,
-            height: 840,
-            title: app.getName(),
-            'web-preferences':
-            {
-                'web-security': false,
-                'overlay-fullscreen-video': true,
-            }
-        }
+	function createWindow() {
+		const windowOptions = {
+			width: 1080,
+			minWidth: 680,
+			minHeight: 680,
+			height: 840,
+			title: app.getName(),
+			'web-preferences': {
+				'web-security': false,
+				'overlay-fullscreen-video': true
+			}
+		};
+		/* @if (process.platform === 'linux') {
+			windowOptions.icon = path.join(__dirname, '/assets/app-icon/png/512.png');
+		} */
 
-        if (process.platform === 'linux')
-        {
-            windowOptions.icon = path.join(__dirname, '/assets/app-icon/png/512.png')
-        }
+		mainWindow = new BrowserWindow(windowOptions);
+		mainWindow.loadURL(path.join('file://', __dirname, '/index.html'));
 
-        mainWindow = new BrowserWindow(windowOptions)
-        mainWindow.loadURL(path.join('file://', __dirname, '/index.html'))
+		// Launch fullscreen with DevTools open, usage: npm run debug
+		if (debug) {
+			mainWindow.webContents.openDevTools();
+			mainWindow.maximize();
+			require('devtron').install();
+		}
 
-        // Launch fullscreen with DevTools open, usage: npm run debug
-        if (debug)
-        {
-            mainWindow.webContents.openDevTools()
-            mainWindow.maximize()
-            require('devtron').install()
-        }
+		mainWindow.on('closed', () => {
+			mainWindow = null;
+		});
 
-        mainWindow.on('closed', function()
-        {
-            mainWindow = null
-        });
+		mainWindow.on('unresponsive', e => {
+			console.log('[Unresponsive]', e);
+		});
 
-        mainWindow.on('unresponsive', function(e)
-        {
-            console.log('[Unresponsive]', e);
-        });
+		mainWindow.webContents.on('crashed', e => {
+			console.log('[Crashed]', e);
+		});
 
-        mainWindow.webContents.on('crashed', function(e)
-        {
-            console.log('[Crashed]', e);
-        });
-
-		mainWindow.webContents.on('will-navigate', function(e)
-		{
+		mainWindow.webContents.on('will-navigate', e => {
 			e.preventDefault();
-		})
+		});
+	}
 
-    }
+	app.on('ready', () => {
+		createWindow();
+		// @autoUpdater.initialize();
+	});
 
-    app.on('ready', function()
-    {
-        createWindow()
-        autoUpdater.initialize()
-    })
+	app.on('window-all-closed', () => {
+		if (process.platform !== 'darwin') {
+			app.quit();
+		}
+	});
 
-    app.on('window-all-closed', function()
-    {
-        if (process.platform !== 'darwin')
-        {
-            app.quit()
-        }
-    })
+	app.on('activate', () => {
+		if (mainWindow === null) {
+			createWindow();
+		}
+	});
 
-    app.on('activate', function()
-    {
-        if (mainWindow === null)
-        {
-            createWindow()
-        }
-    })
-
-    process.on('uncaughtException', function(e)
-    {
-        console.log('[uncaughtException]', e)
-    })
+	process.on('uncaughtException', e => {
+		console.log('[uncaughtException]', e);
+	});
 }
 
 // Make this app a single instance app.
@@ -107,39 +89,40 @@ function initialize()
 //
 // Returns true if the current version of the app should quit instead of
 // launching.
-function makeSingleInstance()
-{
-    if (process.mas) return false
+function makeSingleInstance() {
+	if (process.mas) {
+		return false;
+	}
 
-    return app.makeSingleInstance(function()
-    {
-        if (mainWindow)
-        {
-            if (mainWindow.isMinimized()) mainWindow.restore()
-            mainWindow.focus()
-        }
-    })
+	return app.makeSingleInstance(() => {
+		if (mainWindow) {
+			if (mainWindow.isMinimized()) {
+				mainWindow.restore();
+			}
+
+			mainWindow.focus();
+		}
+	});
 }
 
-// Handle Squirrel on Windows startup events
-switch (process.argv[1])
-{
-    case '--squirrel-install':
-        autoUpdater.createShortcut(function()
-        {
-            app.quit()
-        })
-        break
-    case '--squirrel-uninstall':
-        autoUpdater.removeShortcut(function()
-        {
-            app.quit()
-        })
-        break
-    case '--squirrel-obsolete':
-    case '--squirrel-updated':
-        app.quit()
-        break
-    default:
-        initialize()
-}
+/* // Handle Squirrel on Windows startup events
+switch (process.argv[1]) {
+	case '--squirrel-install':
+		autoUpdater.createShortcut(() => {
+			app.quit();
+		});
+		break;
+	case '--squirrel-uninstall':
+		autoUpdater.removeShortcut(() => {
+			app.quit();
+		});
+		break;
+	case '--squirrel-obsolete':
+	case '--squirrel-updated':
+		app.quit();
+		break;
+	default:
+		initialize();
+} */
+
+initialize();
